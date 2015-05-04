882d70d0099540e82365b3b0ed74494134363f38
Pusher Robot <jack@semantic-ui.com>
2015-05-04 12:59:45 -0400

    Updated component to version 1.12.2

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-sidebar',
   summary : 'Semantic UI - Sidebar: Single component release',
-  version : '1.12.0',
+  version : '1.12.2',
   git     : 'git://github.com/Semantic-Org/UI-Sidebar.git',
 });
 
