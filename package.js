7095bc714c795bffd87a2aec869ad064950091cb
Jack Lukic <jack@semantic-ui.com>
2017-03-29 06:42:57 -0400

    Updated component to version 2.2.10

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-sidebar',
   summary : 'Semantic UI - Sidebar: Single component release',
-  version : '2.2.8',
+  version : '2.2.10',
   git     : 'git://github.com/Semantic-Org/UI-Sidebar.git',
 });
 
